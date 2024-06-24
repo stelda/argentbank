4321e7368caa5506fc1baa3a7c3a3cb22b2bcca4
Cristel <stelda@outlook.fr>
2024-06-24 10:27:28 +0200

    Add error404 page content

---
+++ src/pages/Error404.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/**
+ * Creates a 404 error page.
+ *
+ * @returns {JSX.Element} The JSX element representing the error page.
+ */
+function Error404() {
+    return (
+        <main className='main-content'>
+            <div className="error404">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/">Retourner sur la page d'accueil</a>
+            </div>
+        </main>
+    );
+}
+
+export default Error404;
