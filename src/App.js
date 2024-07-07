47796667f71b0348ffb481fa016f07e6a23c0fd4
stelda <stelda@outlook.fr>
2024-07-07 12:19:41 +0200

    Move App component and adjust import paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import AppRouter from '../AppRouter';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import '../css/main.css';
+import AppRouter from './AppRouter';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './css/main.css';
 
 function App() {
   return (
