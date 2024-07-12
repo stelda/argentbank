ad7b15e89cffbab6ba09ba4ca76e6f66a3ab9025
stelda <stelda@outlook.fr>
2024-07-12 08:10:13 +0200

    Refactor Profile.js to improve readability and functionality

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {getUserProfile} from "../redux/authThunk";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getUserProfile } from "../redux/authThunk";
 import EditName from "../components/EditName";
 
 function Profile() {
@@ -11,16 +11,21 @@ function Profile() {
     const navigate = useNavigate();
     const [isEditing, setIsEditing] = useState(false);
 
+
+    const accountTypes = ["Checking (x8349)", "Savings (x6712)", "Credit Card (x8349)"];
+    const accountBalances = [2082.79, 10928.42, 184.30];
+    const balanceDescriptions = ["Available Balance", "Available Balance", "Current Balance"];
+
     useEffect(() => {
-
+        let isMounted = true;
         if (!token) {
-            navigate('/login');
-        }
-
-        else if (token && user) {
+            if (isMounted) navigate('/login');
+        } else {
             dispatch(getUserProfile(token));
         }
-    }, [dispatch, token, navigate, user]);
+
+        return () => { isMounted = false };
+    }, [dispatch, token, navigate]);
 
     if (!user) {
         return <p>Loading...</p>;
@@ -29,53 +34,31 @@ function Profile() {
     return (
         <main className={`main ${isEditing ? 'bg-light' : 'bg-dark'}`}>
             <div className={`header ${isEditing ? 'welcome' : ''}`}>
-                {isEditing ?
-                    (
-                        <EditName
-                            onCancel={() => setIsEditing(false)}
-                            onSave={() => setIsEditing(false)}
-                        />
-                    )
-                    :
-                    (
-                        <>
-                            <h1>Welcome back<br/>{user.firstName} {user.lastName}!</h1>
-                            <button className="edit-button" onClick={() => setIsEditing(true)}>Edit Name</button>
-                        </>
-                    )
-                }
+                {isEditing ? (
+                    <EditName
+                        onCancel={() => setIsEditing(false)}
+                        onSave={() => setIsEditing(false)}
+                    />
+                ) : (
+                    <>
+                        <h1>Welcome back<br/>{user.firstName} {user.lastName}!</h1>
+                        <button className="edit-button" onClick={() => setIsEditing(true)}>Edit Name</button>
+                    </>
+                )}
             </div>
             <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                    <p className="account-amount">$2,082.79</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className={`transaction-button ${isEditing ? 'transaction-button-active' : ''}`}>View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                    <p className="account-amount">$10,928.42</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className={`transaction-button ${isEditing ? 'transaction-button-active' : ''}`}>View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                    <p className="account-amount">$184.30</p>
-                    <p className="account-amount-description">Current Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className={`transaction-button ${isEditing ? 'transaction-button-active' : ''}`}>View transactions</button>
-                </div>
-            </section>
+            {accountTypes.map((account, index) => (
+                <section key={index} className="account">
+                    <div className="account-content-wrapper">
+                        <h3 className="account-title">Argent Bank {account}</h3>
+                        <p className="account-amount">${accountBalances[index]}</p>
+                        <p className="account-amount-description">{balanceDescriptions[index]}</p>
+                    </div>
+                    <div className="account-content-wrapper cta">
+                        <button className={`transaction-button ${isEditing ? 'transaction-button-active' : ''}`}>View transactions</button>
+                    </div>
+                </section>
+            ))}
         </main>
     );
 }
